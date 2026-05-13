a79cdd844aa1619ee43d0cb755a66cb7bcda1950
Arila Behrami <arila.behrami22@hotmail.com>
2026-05-13 19:33:17 +0200

    Add current user endpoint
    - Added GET /users/me endpoint
    - Protected endpoint with JWT authentication
    - Returns the currently logged-in user

--- routes/user.py
+++ routes/user.py
@@ -1,56 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
-from database import get_db
+from fastapi import APIRouter, Depends
+
 from models.user import User
-from schemas.user import UserRegister, UserResponse
-from security.auth_security import hash_password
+from schemas.user import UserResponse
+from security.auth_security import get_current_user
+
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/", response_model=UserResponse)
-def create_user(user: UserRegister, db: Session = Depends(get_db)):
-    hashed_pw = hash_password(user.password)
-    db_user = User(
-        full_name=user.full_name,
-        email=user.email,
-        hashed_password=hashed_pw
-    )
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
 
-@router.get("/", response_model=list[UserResponse])
-def list_users(db: Session = Depends(get_db)):
-    return db.query(User).all()
-
-@router.get("/{user_id}", response_model=UserResponse)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
-
-@router.put("/{user_id}", response_model=UserResponse)
-def update_user(user_id: int, user_update: UserRegister, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    user.full_name = user_update.full_name
-    user.email = user_update.email
-    user.hashed_password = hash_password(user_update.password)
-
-    db.commit()
-    db.refresh(user)
-    return user
-
-@router.delete("/{user_id}")
-def delete_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    db.delete(user)
-    db.commit()
-    return {"message": f"User with id {user_id} deleted successfully"}
+@router.get("/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
